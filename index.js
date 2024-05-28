c37b0378a624e0d0455da863b6c212f2c1901e88
Gilbert Zaini <11ipa319gilbertzaini@gmail.com>
2024-05-28 09:45:17 +0700

    add error response

--- index.js
+++ index.js
@@ -48,10 +48,12 @@ var uploadFile = multer({ storage: storage, fileFilter: imageFilter });
 app.get("/employees", async (req, res) => {
   try {
     const response = await Employees.findAll();
-    res.status(200).json(response);
+
     console.log(response);
+    return res.status(200).json(response);
   } catch (e) {
     console.log(e.message);
+    return res.status(500).send(e.message);
   }
 });
 
@@ -59,10 +61,12 @@ app.post("/employees", async (req, res) => {
   try {
     console.log(req.body);
     const newEmp = await Employees.create(req.body);
-    res.status(201).json({ msg: "Employee Created", Employees: newEmp });
+
     console.log(response);
+    return res.status(201).json({ msg: "Employee Created", Employees: newEmp });
   } catch (e) {
     console.log(e.message);
+    return res.status(500).send(e.message);
   }
 });
 
@@ -70,27 +74,31 @@ app.post("/employees", async (req, res) => {
 app.get("/tickets", async (req, res) => {
   try {
     const response = await Tickets.findAll();
-    res.status(200).json(response);
+
     console.log(response);
+    return res.status(200).json(response);
   } catch (e) {
     console.log(e.message);
+    return res.status(500).send(e.message);
   }
 });
 
-app.post("/tickets", uploadFile.single('file_path'), async (req, res) => {
+app.post("/tickets", uploadFile.single("file_path"), async (req, res) => {
   try {
     if (req.file) {
-        req.body.file_path = `uploads/${req.file.filename}`;
-      }
+      req.body.file_path = `uploads/${req.file.filename}`;
+    }
     const newTicket = await Tickets.create(req.body);
-    res.status(201).json({ msg: "Ticket Created", Tickets: newTicket });
+
     console.log(response);
+    return res.status(201).json({ msg: "Ticket Created", Tickets: newTicket });
   } catch (e) {
     console.log(e.message);
+    return res.status(500).send(e.message);
   }
 });
 
-app.patch("/tickets/:id", uploadFile.single('file_path'), async (req, res) => {
+app.patch("/tickets/:id", uploadFile.single("file_path"), async (req, res) => {
   try {
     const { assigned_by, status, file_path } = req.body;
     const assigner = await Employees.findByPk(assigned_by);
@@ -110,29 +118,34 @@ app.patch("/tickets/:id", uploadFile.single('file_path'), async (req, res) => {
         },
       });
 
-      res.status(200).json({ msg: "Ticket Updated", Tickets: response });
       console.log(response);
+      return res.status(200).json({ msg: "Ticket Updated", Tickets: response });
     } else {
-      res.status(400).json({
+      return res.status(400).json({
         msg: `${assigner.name}(${assigned_by}) doesn't have the authority`,
       });
     }
   } catch (e) {
     console.log(e.message);
+    return res.status(500).send(e.message);
   }
 });
 
 app.delete("/tickets/:id", async (req, res) => {
   try {
-    await Tickets.destroy({
+    const response = await Tickets.destroy({
       where: {
         id: req.params.id,
       },
     });
-    res.status(200).json({ msg: "Ticket Deleted" });
+
     console.log(response);
+    if (response === 0)
+      return res.status(404).json({ msg: "Ticket not found" });
+    else return res.status(200).json({ msg: "Ticket Deleted" });
   } catch (e) {
     console.log(e.message);
+    return res.status(500).send(e.message);
   }
 });
 
