33f335d1f8dd979e1a2a67580fc6acbcdcaed5ec
Gilbert Zaini <gilbertzaini03@gmail.com>
2024-05-29 17:05:20 +0700

    add files model

---
+++ models/files.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class files extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  files.init({
+    name: DataTypes.STRING,
+    ticket_id: DataTypes.STRING,
+    is_deleted: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'files',
+  });
+  return files;
+};
